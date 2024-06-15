c2f846de37c69874c3f2eac7180733b8eba227ed
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2024-06-15 22:20:30 +0530

    Modified footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@
 export default function Footer() {
     return (
         <footer>
+            <div className="bgGradient"></div>
             <div>
                 <h2>The Harsh Martian Land</h2>
                 <h1>The APOD Project</h1>
