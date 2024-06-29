578c6fa50a20076134efd4340efbe32dc4e1b6d1
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2024-06-29 12:38:41 +0530

    siderbar modified

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,19 @@
 
 export default function SideBar() {
     return (
-        <div>
-            <h2>The Harsh Martian Land</h2>
-            <div>
-                <p>Description</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Harsh Martian Land</h2>
+                <div>
+                    <p>Description</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <button>
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>
             </div>
+
         </div>
     )
 }
